c912726f38fb5391c6697c3de8f4720f8c06ba17
Zatorski <lukasz.zatorskii@gmail.com>
2019-10-01 19:20:16 +0200

    Add PasswordChange component

--- src/components/PasswordChange/index.tsx
+++ src/components/PasswordChange/index.tsx
@@ -1,8 +1,56 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { FirebaseContext } from '../Firebase';
 
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-);
-export default PasswordChange;
+const PasswordChangeForm = () => {
+  const firebase = useContext(FirebaseContext);
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          password: '',
+          passwordConfirmation: '',
+        }}
+        validate={values => {
+          let errors: any = {};
+          if (!values.password) {
+            errors.password = 'Password required';
+          } else if (values.password !== values.passwordConfirmation) {
+            errors.passwordConfirmation = 'Must match password';
+          }
+          return errors;
+        }}
+        onSubmit={(values, actions) => {
+          firebase!.doPasswordUpdate(values.password).then(
+            () => {
+              actions.setSubmitting(false);
+              actions.resetForm();
+            },
+            error => {
+              actions.setSubmitting(false);
+              actions.setStatus({ msg: error.message });
+            },
+          );
+        }}
+        render={({ status, isValid, isSubmitting }) => (
+          <Form>
+            <Field type='password' name='password' placeholder='Password' />
+            <ErrorMessage name='password' component='div' />
+            <Field
+              type='password'
+              name='passwordConfirmation'
+              placeholder='Confirm Password'
+            />
+            <ErrorMessage name='passwordConfirmation' component='div' />
+            {status && status.msg && <div>{status.msg}</div>}
+            <button type='submit' disabled={isSubmitting || !isValid}>
+              Change My Password
+            </button>
+          </Form>
+        )}
+      />
+    </div>
+  );
+};
+
+export default PasswordChangeForm;
